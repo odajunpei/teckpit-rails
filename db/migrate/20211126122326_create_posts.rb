d31b519378756e5efb170bda796fefedaae9c156
odajunpei <i.penju1022@gmail.com>
2021-12-05 13:40:10 +0000

    fix

--- db/migrate/20211126122326_create_posts.rb
+++ db/migrate/20211126122326_create_posts.rb
@@ -1,9 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-            t.string :title
+      t.string :title
       t.text :body
       t.string :video
+      t.string :image
       t.timestamps
     end
   end
